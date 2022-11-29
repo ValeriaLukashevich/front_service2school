ad439aec6a5f57ca7e02fd90d1d9035c6961980b
admin <vaher.kaupo@gmail.com>
2022-11-29 16:38:04 +0200

    DropdownView.vue connected with back - done

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Login</h3>
+    <div class="row justify-container-center">
+      <div class="col-lg-5">
+        <div class="alert alert-danger" role="alert">
+
+        </div>
+        <div class="input-group mb-3">
+          <span class="input"
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
