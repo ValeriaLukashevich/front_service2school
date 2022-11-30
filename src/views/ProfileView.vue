6d552a903d55ec0bf23be5101b8453d2a1a09b86
Valeria <valeria.lukashevich@gmail.com>
2022-11-30 09:31:22 +0200

    Commit

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+  <div>
+    <h1>Minu konto info:</h1>
+    <h2>
+
+    </h2>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileView",
+  data: function () {
+    return {
+      contact: [
+        {
+          contactId: 0,
+          contactFirstName: '',
+          contactLastName: '',
+          contactPhone: '',
+          contactInstitution: '',
+
+        }
+      ]
+    }
+
+  },
+  methods: {
+    getContactInfo: function () {
+      this.$http.get("/contact")
+          .then(response => {
+            this.contact = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+    beforeMount() {
+      this.getContactInfo()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
