e0a3e33679518e4efd864cceeb0187a53c577c97
admin <vaher.kaupo@gmail.com>
2022-11-30 09:21:51 +0200

    login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,15 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <template>
   <div>
     <h3>Login</h3>
-    <div class="row justify-container-center">
+    <div class="button">
 
-      <div class="input-group mb-3">
+
+      <div>
         <span class="input-group-text" id="basic-addon1">email</span>
         <input v-model="email" type="text" class="form-control" placeholder="email" aria-label="email" aria-describedby="basic-addon1">
       </div>
-      <div class="input-group mb-3">
+      <div>
         <span class="input-group-text" id="basic-addon1">password</span>
         <input v-model="password" type="text" class="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon1">
       </div>
