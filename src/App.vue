8c73fdd90fdc62bcfde836d128a1419740f15f75
Valeria <valeria.lukashevich@gmail.com>
2022-12-07 09:15:32 +0200

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,38 @@
 <template>
-    <div id="app">
-          <nav>
-            <router-link to="/" type="button" class="btn btn-outline-success">Home</router-link>
-            <router-link to="/about" type="button" class="btn btn-outline-success">Teenused</router-link>
-            <router-link to="/loginRequest" type="button" class="btn btn-outline-success">Login</router-link>
-          </nav>
-          <router-view/>
 
-    </div>
+  <div id="app">
+    <nav>
+      <router-link to="/" type="button" class="btn btn-outline-success">Kodu</router-link>
+      <router-link to="/about" type="button" class="btn btn-outline-success">Teenused</router-link>
 
+      <router-link to="/profile" v-if="requestUser"  type="button" class="btn btn-outline-success">Konto</router-link>
+
+      <router-link to="/loginRequest" v-else type="button" class="btn btn-outline-success">Login</router-link>
+
+    </nav>
+    <router-view/>
+  </div>
 
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
+export default {
+
+  data() {
+    return {
+      requestUser: null,
+    }
+  },
+  created() {
+    this.setRequestUser();
+  },
+methods: {
+  setRequestUser() {
+    this.requestUser = window.sessionStorage.getItem('userId')
+  }
 }
-</style>
-<script>export default {
-  components: {}
 }
 </script>
